05b9238ae4548af7b2c3252ed0f04fadac0136ce
Anshu2Upadhyay <imanshuupadhyay@gmail.com>
2026-06-27 10:27:50 +0530

    update student model

--- models/student.js
+++ models/student.js
@@ -5,4 +5,13 @@ const studentSchema = new mogoose.Schema({
     timestamps: true
 });
 
+module.exports = mogoose.model('Student', studentSchema({
+    name:{},
+    email:{},
+    city:{},
+    age:{},
+}, {
+    timestamps: true
+}));
+
 module.exports = mogoose.model('Student', studentSchema);
